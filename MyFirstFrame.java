e39e893ba8264a7d1c4b82fb44bd1e720eba9a86
shruti01052002 <shrutiyadav26072002@gail.com>
2021-05-05 21:34:55 +0530

    JFrame

---
+++ MyFirstFrame.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+class MyFirstFrame{
+	public static void main(String args[]){
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frame.setSize(700, 500);
+		//frame.setLocation(100, 50);
+		frame.setBounds(100, 100, 1000, 500);
+		frame.setTitle("My Frame");
+		
+		ImageIcon icon = new ImageIcon("small-icon.jpg");
+		frame.setIconImage(icon.getImage());
+		
+		Container c = frame.getContentPane();
+		//Color color = new Color(255, 0, 0);
+		c.setBackground(Color.BLUE);
+		
+		frame.setResizable(false); //We can't resize the GUI
+	}
+}
